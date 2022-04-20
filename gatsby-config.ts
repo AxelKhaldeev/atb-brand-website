20935e03459658cff9d297da151fa8e6c18bfd5c
Alex <khaldeev@gmail.com>
2022-04-20 12:34:48 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,28 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Atb Brand`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "000"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
